f71a815501d93561c2485d6bf76daa456d12b372
Glenn Seplowitz <fredzachrcool@optonline.net>
2016-08-23 20:54:46 -0400

    Added contoso site with EF.

--- ContosoSite/Properties/AssemblyInfo.cs
+++ ContosoSite/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6dd97a3b-7a89-4d38-aa88-0fdaf5648986")]
+[assembly: Guid("31a853d4-3855-4150-8842-35bb5a1a7147")]
 
 // Version information for an assembly consists of the following four values:
 //
